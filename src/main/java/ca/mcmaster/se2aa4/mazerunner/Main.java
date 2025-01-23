e7bc387b4baf41b7cf31f8b8a84612db10ae7798
Ibrs21 <ibrsboy@gmail.com>
2025-01-23 17:31:05 -0500

    Initial commit

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -0,0 +1,40 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger();
+
+    public static void main(String[] args) {
+        logger.info("** Starting Maze Runner");
+        try {
+            if (args.length ==2 && args[0].equals("-i")){
+                logger.info("**** Reading the maze from file " + args[1]);
+                BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    for (int idx = 0; idx < line.length(); idx++) {
+                        if (line.charAt(idx) == '#') {
+                            logger.trace("WALL ");
+                        } else if (line.charAt(idx) == ' ') {
+                            logger.trace("PASS ");
+                        }
+                    }
+                    logger.trace(System.lineSeparator());
+                }
+            }
+            else throw new Exception();
+            
+        } catch(Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
+        }
+        logger.info("**** Computing path");
+        logger.info("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
+    }
+}
